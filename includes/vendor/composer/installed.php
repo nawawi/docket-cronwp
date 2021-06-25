bd75bbc523ffedbc44478f8359a413c783733ab4
Nawawi Jamili <nawawi@rutweb.com>
2021-06-26 00:45:38 +0800

    fixed: composer.json

--- includes/vendor/composer/installed.php
+++ includes/vendor/composer/installed.php
@@ -1,24 +1,24 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => '5f3ee9df8bb95ce22cbd9927092373b46bbc97ce',
     'name' => 'nawawi/docket-cronwp',
   ),
   'versions' => 
   array (
     'nawawi/docket-cronwp' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => '5f3ee9df8bb95ce22cbd9927092373b46bbc97ce',
     ),
   ),
 );
